7439ebe86cf258caf33911ea419585b94a1589a2
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2022-03-15 19:08:05 +0100

    update

--- src/App.js
+++ src/App.js
@@ -4,51 +4,65 @@ import './App.css';
 function App() {
   const [text, setText] = useState("");
   const [tasks, setTasks] = useState([
-    { label: "Learn HTML" },
-    { label: "Learn CSS" },
-    { label: "Learn JS" },
+    { completed: false, label: "Learn HTML" },
+    { completed: false, label: "Learn CSS" },
+    { completed: false, label: "Learn JS" },
 
   ]);
 
+  const taskElements = tasks.map((task, index) => {
+    const onClickTask = () => {
+      setTasks([
+        ...tasks.slice(0, index),
+        {
+          ...task,
+          completed: !task.completed,
+        },
+        ...tasks.slice(index + 1),
+      ]);
+    };
 
-  const taskElements = tasks.map(task => {
-    return (
-      <li key={task.label}>
-        <span className='label'>{task.label}</span>
-      </li>
-    );
+    const onClickTrashcan = () => {
+      setTasks(tasks.filter((taskToFilter) => taskToFilter !== task));
+    };
+
+return (
+  <li key={task.label} className={task.completed ? "completed": null}>
+    <span className='label' onClick={onClickTask}>{task.label}</span>
+  </li>
+);
   });
 
-  const onclickOk = () => {
-    setText("");
-    setTasks([
-      ...tasks, {
-        label: text,
-      },
-    ]);
-  };
+const onclickOk = () => {
+  setText("");
+  setTasks([
+    ...tasks, {
+      label: text,
+    },
+  ]);
+};
 
-  const onTextChange = (event)=>{
-    setText(event.target.value);
-  };
+const onTextChange = (event) => {
+  setText(event.target.value);
+};
 
-  
-  return (
-    <div className="App">
-      <div className='container'>
-        <h1>TODO</h1>
-        <p className='counter'>0 completed</p>
-        <input type='text'
-          value={text}
-          onChange={onTextChange} />
-        <button onClick={onclickOk}>OK</button>
-        <small className='error' />
-        <ul>
-          {taskElements}
-        </ul>
-      </div>
+
+return (
+  <div className="App">
+    <div className='container'>
+      <h1>TODO</h1>
+      <p className='counter'>0 completed</p>
+      <input type='text'
+        value={text}
+        onChange={onTextChange} />
+      <button onClick={onclickOk}>OK</button>
+      <small className='error' />
+      <ul>
+        {taskElements}
+      </ul>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
