bc074ea005f0b408335f8f05f758c95b480cc1f1
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2022-03-15 16:25:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className='container'>
-        <h1>TODO</h1>
-        <p className='counter'>0 completed</p>
-        <input type='text'></input>
-        <button>OK</button>
-        <small className='error' />
-        <ul>
-          <li><span className='Label'>Learn HTML</span>  </li>
-          <li><span className='Trashcan'>Learn CSS</span></li>
-        </ul>
-      </div>
+  const tasks = [{ label: "Learn HTML" }, { label: "Learn CSS" }, {label: "Learn JS"}];
+
+  // const taskElements= [
+  //   <li>
+  //   <span className='Label'>Learn HTML</span> 
+  //   </li>,
+  //   <li>
+  //   <span className='Trashcan'>Learn CSS</span>
+  //   </li>,
+  // ];
+  //Vi gör om den:
+
+  const taskElements = tasks.map(task => {
+    return (
+      <li key={task.label}>
+        <span className='label'>{task.label}</span>
+      </li>
+  
+   );
+});
+
+return ( 
+  <div className="App">
+    <div className='container'>
+      <h1>TODO</h1>
+      <p className='counter'>0 completed</p>
+      <input type='text'></input>
+      <button>OK</button>
+      <small className='error' />
+      <ul>
+        {taskElements}
+      </ul>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
