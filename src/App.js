c71f6f547a712a835643c0779fb314553073677b
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2022-03-15 13:49:42 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>TODO</h1>
+      <div className='container'>
+        <h1>TODO</h1>
+        <p className='counter'>0 completed</p>
+        <input type='text'></input>
+        <button>OK</button>
+        <small className='error' />
+        <ul>
+          <li><span className='Label'>Learn HTML</span>  </li>
+          <li><span className='Trashcan'>Learn CSS</span></li>
+        </ul>
+      </div>
     </div>
   );
 }
